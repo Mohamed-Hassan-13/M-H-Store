f9ccac26fa5638c3c67a42b9b5123003271cf84a
Hamo <moahmedhassan21700@gmail.com>
2025-01-07 17:03:01 +0200

    ubdate5

--- src/Components/website/Products/LatestSaleProduct.js
+++ src/Components/website/Products/LatestSaleProduct.js
@@ -22,7 +22,10 @@ export default function LatestSale() {
       key={index}
       title={item.title}
       description={item.description}
-      image={item.images[0].image}
+      image={
+        "https://m-h-store-backend-production.up.railway.app" +
+        item.images[0].image
+      }
       rating={item.rating}
       sale
       price={item.price}
